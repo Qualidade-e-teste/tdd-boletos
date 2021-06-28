0fbcbe7a6016e835b097bb818a39aa59a2bbc4d6
marcosvor <marcos_ribeiro@outlook.com>
2021-06-27 22:11:29 -0300

    Adjusting tests from processamento

--- Test/boleto/BoletoTest.java
+++ Test/boleto/BoletoTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import processador.Boleto;
-
 class BoletoTest {
 	
 	static Boleto boleto;
--- Test/processador/ProcessadorTest.java
+++ Test/processador/ProcessadorTest.java
@@ -4,13 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import boleto.Boleto;
+import fatura.Fatura;
 
 class ProcessadorTest {
 
@@ -27,16 +28,20 @@ class ProcessadorTest {
 		fatura1 = new Fatura("27/06/2021", 1500.00, "Cliente1");
 		fatura2 = new Fatura("27/06/2021", 1500.00, "Cliente2");
 		fatura3 = new Fatura("27/06/2021", 2000.00, "Cliente3");
-		processador = new Processador();
 	}
 
+	@BeforeEach
+	void setUpBeforeEach() throws Exception {
+		processador = new Processador();
+	}
+	
 	@DisplayName("Soma dos boletos igual ao valor da fatura")
 	@Test
 	void testProcessamento1() {
 		boleto1 = new Boleto(2021210601, "21/06/2021", 500.00);
 		boleto2 = new Boleto(2021220602, "22/06/2021", 400.00);
 		boleto3 = new Boleto(2021230603, "23/06/2021", 600.00);
-		ArrayList boletos = new ArrayList();
+		ArrayList<Boleto> boletos = new ArrayList<Boleto>();
 		boletos.add(boleto1);
 		boletos.add(boleto2);
 		boletos.add(boleto3);
@@ -50,6 +55,12 @@ class ProcessadorTest {
 		boleto1 = new Boleto(2021210601, "21/06/2021", 1000.00);
 		boleto2 = new Boleto(2021220602, "22/06/2021", 500.00);
 		boleto3 = new Boleto(2021230603, "23/06/2021", 250.00);
+		ArrayList<Boleto> boletos = new ArrayList<Boleto>();
+		boletos.add(boleto1);
+		boletos.add(boleto2);
+		boletos.add(boleto3);
+		processador.processaPagamentos(boletos, fatura2);
+		Assertions.assertAll(()-> assertEquals("PAGA", fatura2.getStatus()), () -> assertEquals(3, processador.getQtdePagamentos()));
 	}
 
 	@DisplayName("Soma dos boletos inferior ao valor da fatura")
@@ -57,6 +68,11 @@ class ProcessadorTest {
 	void testProcessamento3() {
 		boleto1 = new Boleto(2021210601, "21/06/2021", 500.00);
 		boleto2 = new Boleto(2021220602, "22/06/2021", 400.00);
+		ArrayList<Boleto> boletos = new ArrayList<Boleto>();
+		boletos.add(boleto1);
+		boletos.add(boleto2);
+		processador.processaPagamentos(boletos, fatura3);
+		Assertions.assertAll(()-> assertNull(fatura3.getStatus()), () -> assertEquals(2, processador.getQtdePagamentos()));
 	}
 
 }
